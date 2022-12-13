e146e8e78d835f5db885fdd07709661dd67883d9
Leon Si <leon@leondreamed.com>
2022-12-13 10:58:49 -0500

    feat: publish until version 200

--- packages/scripts/autopublish.ts
+++ packages/scripts/autopublish.ts
@@ -5,7 +5,7 @@ import { getProjectDir } from 'lionconfig';
 const monorepoDir = getProjectDir(import.meta.url, { monorepoRoot: true });
 const depignorePath = path.join(monorepoDir, 'packages/ignoredep');
 
-for (let majorVersion = 26; majorVersion <= 200; majorVersion += 1) {
+for (let majorVersion = 114; majorVersion <= 200; majorVersion += 1) {
 	exec(`pnpm version ${majorVersion}.9999.0`, { cwd: depignorePath });
 	exec('pnpm publish --no-git-checks', { cwd: depignorePath, stdio: 'inherit' });
 }
